fe15250b64a09b6d72b905a75fd5a143c8bf4dd7
JoseLuis10000 <91105322+JoseLuis10000@users.noreply.github.com>
2022-11-19 17:04:33 -0600

    Update sumaVector.asm

--- Proyecto/Tarea2/sumaVector.asm
+++ Proyecto/Tarea2/sumaVector.asm
@@ -2,8 +2,8 @@
     align 16
     sumaVectorial proc
     
-    vmovups xmm0, xmmword ptr [rbx]
-    vmovups xmm1, xmmword ptr [rcx]
+    vmovups xmm0, xmmword ptr [rcx]
+    vmovups xmm1, xmmword ptr [rdx]
     vaddps  xmm2, xmm1, xmm0
 
     ret
