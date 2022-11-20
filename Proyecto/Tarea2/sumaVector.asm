e1e4723bc4dd446c1ea5180e96994ffd8bf3b518
JoseLuis10000 <91105322+JoseLuis10000@users.noreply.github.com>
2022-11-20 17:09:48 -0600

    Update sumaVector.asm

--- Proyecto/Tarea2/sumaVector.asm
+++ Proyecto/Tarea2/sumaVector.asm
@@ -5,7 +5,7 @@
     vmovups ymm1, ymmword ptr [rcx]
     vmovups ymm2, ymmword ptr [rdx]
     vaddps  ymm0, ymm1, ymm2
-    vmovq eax, dword ptr xmm1
+    vmovd eax, dword ptr xmm1
     ret
     sumaVectorial endp
 end
