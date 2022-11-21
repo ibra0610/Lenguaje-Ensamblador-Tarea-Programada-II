c300187947cd28d2c358cb784df55f87496492fd
JoseLuis10000 <91105322+JoseLuis10000@users.noreply.github.com>
2022-11-20 18:38:37 -0600

    Update sumaVectorial.asm

--- Tarea2-Ensamblador/Tarea2-Ensamblador/sumaVectorial.asm
+++ Tarea2-Ensamblador/Tarea2-Ensamblador/sumaVectorial.asm
@@ -5,7 +5,7 @@
     movups xmm1, xmmword ptr [rcx]
     movups xmm2, xmmword ptr [rdx]
     addps  xmm0, xmm1, xmm2
-    movd eax, xmm0
+    movups xmmword ptr [r8], xmm0
     ret
     sumaVectorial endp
 end
